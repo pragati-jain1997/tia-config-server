e1437a96f09f6b148a0e0a93da800ee1becf2a21
pragati jain <pragati.jain.126@gmail.com>
2025-10-22 21:55:21 +0530

    Initial commit for config server (reimported project)

---
+++ src/test/java/com/tiahealth/tia_config_server/TiaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiahealth.tia_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
